367263b38f060dec00a14cc0f11e995c8bc95ef1
droll-accelerator-1483 <srsani@gmail.com>
2019-07-21 12:46:09 +0000

    Automatically backed up by Learn

--- lib/square_array.rb
+++ lib/square_array.rb
@@ -1,3 +1,9 @@
 def square_array(array)
-  # your code here
+  counter = 0
+  new_array = []
+  while array[counter] do
+    new_array.append(array[counter]**2)
+    counter += 1
+  end
+  return new_array
 end
